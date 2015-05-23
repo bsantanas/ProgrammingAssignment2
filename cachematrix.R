0b1fa04b868c91235a9e637b140bfc6223ca6dc4
bsantanas <bsantana@hellobundle.com>
2015-05-23 12:55:24 +0100

    Bernardo Santana. R Programming course, Programming assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix contains utility functions to optimize the calculation of
+## a matrix inverse, storing this value in cache and therefore avoiding 
+## the need to calculate it multiple times
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list object containing a matrix
+## and the value of its inverse stored both in cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve Attempts to get the inverse of the supplied matrix from cache,
+## if it doesn't exist, it calculates and stores the value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinv(inv)
+  
+  # Return a matrix that is the inverse of 'x'
+  inv
 }
+
+testsolve <- function() {
+  x <- matrix(runif(16,1,10),4,4)
+  mymatrix <- makeCacheMatrix(x)
+  for i <- 1:3 {
+    cacheSolve(mymatrix)
+  }
+}
+
